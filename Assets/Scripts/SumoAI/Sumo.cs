bff0d0d23fe2138f822efb6e00e2a0aa59f1149a
Eyal Rufeisen <eyalrocks@gmail.com>
2021-03-05 18:18:17 +0100

    Eyal&Razvan baseAI stuff

---
+++ Assets/Scripts/SumoAI/Sumo.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sumo : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Collider coll;
+
+    public bool actionLocked;
+
+    [Header("Movement")]
+    public float movementSpeed;
+    //public Vector3 moveDestination;
+
+    [Header("Rotation")]
+    public float rotationSpeed;
+
+    [Header("Pushing")]
+    public bool isPushing;
+    public float pushCD;
+    public float pushDuration;
+    public float pushForce;
+
+    [Header("Dodge")]
+    public bool isDodging;
+    public float dodgeCD;
+    public float dodgeDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<CapsuleCollider>();
+    }
+
+    public void Movement()
+    {
+        rb.MovePosition(rb.position + (transform.forward * movementSpeed * Time.deltaTime));
+    }
+
+    public void Rotate(float rotationAngle)
+    {
+        //Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, rotationAngle, 0) * rotationSpeed * Time.deltaTime);
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, new Vector3(0, rotationAngle, 0), rotationSpeed * Time.deltaTime, 0);
+        transform.rotation = (Quaternion.LookRotation(newDirection));
+    }
+
+    public void Dodge()
+    {
+        // Doing the dodge
+        coll.enabled = false;
+
+        // State changes
+        isDodging = true;
+        actionLocked = true;
+        StartCoroutine(ActionLock(dodgeDuration));
+        StartCoroutine(DodgeCooldown());
+    }
+    public void Push()
+    {
+        // Performing the push
+        rb.AddForce(transform.forward * pushForce, ForceMode.Acceleration);
+
+        // State changes
+        isPushing = true;
+        actionLocked = true;
+        StartCoroutine(ActionLock(pushDuration));
+        StartCoroutine(PushCooldown());
+    }
+
+    IEnumerator ActionLock(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        actionLocked = false;
+    }
+
+    IEnumerator DodgeCooldown()
+    {
+        yield return new WaitForSeconds(dodgeCD);
+        isDodging = false;
+        coll.enabled = true;
+    }
+
+    IEnumerator PushCooldown()
+    {
+        yield return new WaitForSeconds(pushCD);
+        isPushing = false;
+    }
+}
---
+++ Assets/Scripts/SumoAI/SumoBaseAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumoBaseAI : Sumo
+{
+    [Header("AI")]
+    public AiObjective currObjective;
+    public float rotateTo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PerformObjective();
+    }
+
+    void PerformObjective ()
+    {
+        if (!actionLocked)
+        {
+            if (currObjective == AiObjective.push && !isPushing)
+            {
+                Push();
+            }
+            else if (currObjective == AiObjective.dodge && !isDodging)
+            {
+                Dodge();
+            }
+            else if (currObjective == AiObjective.rotate)
+            {
+                // based on rotationDir decides 
+                //float myAngle = transform.eulerAngles.y % 360; // 100
+                //float destinationAngle = rotateTo % 360; // 200
+                //float rotationDir = ((myAngle - destinationAngle) % 360) >= 180 ? -1 : 1;
+
+                // if they're not really close
+                //if (Mathf.Abs(myAngle - destinationAngle) > 0.1f)
+                //{
+                    Rotate(rotateTo);
+                //}
+            }
+            else if (currObjective == AiObjective.move)
+            {
+                Movement();
+            }
+            else if (currObjective == AiObjective.wait)
+            {
+                // Do nothing :D
+            }
+        }
+    }
+}
