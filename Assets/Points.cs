2a2dd92573418a1b7be9991b8d2ccff9e8c3bcfa
J3F31 <72414263+J3F31@users.noreply.github.com>
2021-03-05 16:54:12 +0100

    Spawning & Manager v1

---
+++ Assets/Points.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+    int points;
+    bool inCircle;
+    Coroutine inside;
+    [SerializeField] Text pointCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        inside = StartCoroutine(PointCycle());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StopCoroutine(inside);
+    }
+
+    IEnumerator PointCycle()
+    {
+        yield return new WaitForSeconds(0.5f);
+        points++;
+        pointCount.text = points.ToString();
+        inside = StartCoroutine("PointCycle");
+    }
+}
+
