32cf8fa6f62e33515d32c9f4c00ac211ebf6ffce
Eyal Rufeisen <eyalrocks@gmail.com>
2021-03-06 16:24:57 +0100

    Fixed rotations & pushing

--- Assets/Scripts/SumoAI/SumoBaseAI.cs
+++ Assets/Scripts/SumoAI/SumoBaseAI.cs
@@ -6,7 +6,7 @@ public class SumoBaseAI : Sumo
 {
     [Header("AI")]
     public AiObjective currObjective;
-    public float rotateTo;
+    public float rotateToY;
 
     // Update is called once per frame
     void Update()
@@ -28,16 +28,7 @@ public class SumoBaseAI : Sumo
             }
             else if (currObjective == AiObjective.rotate)
             {
-                // based on rotationDir decides 
-                //float myAngle = transform.eulerAngles.y % 360; // 100
-                //float destinationAngle = rotateTo % 360; // 200
-                //float rotationDir = ((myAngle - destinationAngle) % 360) >= 180 ? -1 : 1;
-
-                // if they're not really close
-                //if (Mathf.Abs(myAngle - destinationAngle) > 0.1f)
-                //{
-                    Rotate(rotateTo);
-                //}
+                Rotate(rotateToY);
             }
             else if (currObjective == AiObjective.move)
             {
