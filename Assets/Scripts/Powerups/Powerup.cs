74911d639b53b72a65a668361e5a9d3319705199
Eyal Rufeisen <eyalrocks@gmail.com>
2021-03-23 14:56:10 +0100

    Changed powerups to be under one base powerup class + implemented polished camera

---
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Collider other = collision.collider;
+        if (other.tag == "Player")
+        {
+            Sumo sumo = other.GetComponent<Sumo>();
+            OnSumoCollision(sumo);
+        }
+    }
+
+    protected virtual void OnSumoCollision(Sumo somu)
+    {
+
+    }
+}
