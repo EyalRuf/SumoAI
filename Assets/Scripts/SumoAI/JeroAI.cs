6dc83636f3c8655cae7760fd79f41fc951c9b59e
J3F31 <72414263+J3F31@users.noreply.github.com>
2021-03-24 15:16:30 +0100

    UI and JeroAI

--- Assets/Scripts/SumoAI/JeroAI.cs
+++ Assets/Scripts/SumoAI/JeroAI.cs
@@ -1,42 +1,156 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class JeroAI : SumoBaseAI
 {
-    private SumoBaseAI[] enemySumos;
+    //PoweUp vars
+    private List<GameObject> pointPowerUps;
+    private GameObject pointPower;
+    private GameObject closestPointPowerUp;
+
+    //Player vars
+    private List<SumoBaseAI> allEnemySumos;
+    private GameObject closestSumo;
+
+    //Ring vars
     private GameObject ring;
+    private float ringDistance;
 
-    // Start is called before the first frame update
-    public override void Start()
-    {
-        base.Start();
-
-        this.currObjective = AiObjective.idle;
-        enemySumos = FindObjectsOfType<SumoBaseAI>();
-        ring = GameObject.Find("ObjectiveCircle");
-    }
+    //Rotation vars
+    private bool ToRing;
+    private bool ToPlayer;
+    private bool ToPowerUp;
 
     // Update is called once per frame
     void Update()
     {
-        this.destination = new Vector2(ring.transform.position.x, ring.transform.position.z);
-        this.rotateToY = Quaternion.LookRotation(ring.transform.position - transform.position).eulerAngles.y;
+        GetRing();
+        GetSumo();
+        
 
-        foreach (SumoBaseAI enemySumo in enemySumos)
+        if (ringDistance < 5f) //ring bounds to be implemented?
         {
-            float range = Vector2.Distance(transform.position, enemySumo.transform.position);
-
-            Debug.Log(range);
-
-            if (range <= 1 && range > 0)
+            if (closestSumo && Vector3.Distance(closestSumo.transform.position, transform.position) < 1f)
             {
-                this.currObjective = AiObjective.dodge;
+                ObjectivePlayer(closestSumo);
             }
-            else
+
+            GetPowerUps();
+            
+        }
+        else
+        {
+            ObjectiveRing(ring);
+        }
+
+        foreach (SumoBaseAI sumo in allEnemySumos)
+        {
+            if (Vector3.Distance(sumo.transform.position, transform.position) < 2f)
             {
-                this.currObjective = AiObjective.idle;
+                closestSumo = sumo.gameObject;
             }
         }
+
+        foreach(GameObject pointPower in pointPowerUps)
+        {
+            if (Vector3.Distance(pointPower.transform.position, transform.position) < 1f)
+            {
+                closestPointPowerUp = pointPower;
+                
+            }
+        }
+
+        if (ToRing)
+        {
+            this.rotateToY = Quaternion.LookRotation(ring.transform.position - transform.position).eulerAngles.y;
+            Debug.Log("Rotating to ring");
+        }
+        else if (ToPlayer)
+        {
+            this.rotateToY = Quaternion.LookRotation(closestSumo.transform.position - transform.position).eulerAngles.y;
+            Debug.Log("Rotating to player");
+        }
+        else if (ToPowerUp)
+        {
+            this.rotateToY = Quaternion.LookRotation(closestPointPowerUp.transform.position - transform.position).eulerAngles.y;
+        }
+    }
+
+    void ObjectiveRing(GameObject ring)
+    {
+        Debug.Log("ObjectiveRing");
+        ToRing = true;
+        ToPlayer = false;
+        ToPowerUp = false;
+
+        StartCoroutine(DelayedPush(ring));
+    }
+
+    void ObjectivePlayer(GameObject sumo)
+    {
+        ToRing = false;
+        ToPlayer = true;
+        ToPowerUp = false;
+
+        StartCoroutine(DelayedPush(sumo));
+    }
+
+    void ObjectivePowerUp(GameObject powerUp)
+    {
+        ToRing = false;
+        ToPlayer = false;
+        ToPowerUp = true;
+
+        StartCoroutine(DelayedPush(powerUp));
+    }
+
+    void ObjectiveSelf()
+    {
+        Debug.Log("ObjectiveSelf");
+        if (closestSumo.GetComponent<Sumo>().isPushing)
+        {
+            this.currObjective = AiObjective.dodge;
+        }
+        this.destination = new Vector2(transform.position.x, transform.position.z);
+    }
+
+    void GetRing()
+    { 
+        ring = GameObject.Find("ObjectiveCircle");  
+
+        ringDistance = Vector3.Distance(ring.transform.position, transform.position);
+    }
+
+    void GetSumo()
+    {
+        allEnemySumos = GameObject.FindObjectsOfType<SumoBaseAI>().ToList();
+        allEnemySumos.Remove(this);
+    }
+
+    void GetPowerUps()
+    {
+        pointPower = GameObject.Find("PowerupPoints(Clone)");
+        if(pointPowerUps.Contains(pointPower))
+        {
+            pointPowerUps.Add(pointPower);
+        }
+    }
+
+    IEnumerator DelayedPush(GameObject dest) 
+    {
+        this.destination = new Vector2(dest.transform.position.x, dest.transform.position.z);
+        this.currObjective = AiObjective.idle;
+
+        yield return new WaitForSeconds(2f);
+
+        this.currObjective = AiObjective.push;
+
+        closestSumo = null;
+
+        yield return new WaitForSeconds(0.1f);
+
+        this.currObjective = AiObjective.idle;
     }
 }
