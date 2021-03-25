f61bf784f22f4498e1dc303e868a8ea7f2fb8b1d
Kullcord <58976043+Kullcord@users.noreply.github.com>
2021-03-25 13:41:21 +0000

    latest push

--- Assets/Scripts/SumoAI/RazvanAI.cs
+++ Assets/Scripts/SumoAI/RazvanAI.cs
@@ -98,10 +98,10 @@ public class RazvanAI : SumoBaseAI
             {
                 if(distanceUntillRing < distanceUntillPowerUp)
                 {
-
+                    destination = distanceUntillPowerUp.position;
                 } else
                 {
-
+                    destination = ringCenter;
                 }
             }*/
             
